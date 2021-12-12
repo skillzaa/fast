fd1fc9ab17e0ebf64fbfa93db3b6e858cf86ad93
skilzaa <skillzaa.com@gmail.com>
2021-12-12 11:51:37 +0500

    i can send text

--- src/main.rs
+++ src/main.rs
@@ -7,54 +7,72 @@ fn main() {
     let a = app::App::default().with_scheme(app::Scheme::Gtk);
 
     let mut win = window::Window::default().with_size(640, 480);
+
     let mut flow = Flow::default_fill();
-    //============== widgets
-//......................urow
-
-
-    // let mut f = frame::Frame::default()
-    //     .with_label("Username:");
-    // f.set_label_size(25);
-       let f = 
-       written_text("delet ethe text",44); 
-
-
-//......................urow
-
+// -----------------------------------------   
+    //============== level zero
+    let hdg = heading();
+    let hdg_sep = sep();
+    //============== level one
+    let mut change_data_btn = change_data_btn("change data");
     let inp = input::Input::default().with_size(150, 30);
-    let btn_change_data = button::Button::default().with_size(100, 30).with_label("Change");
+    let mut explain = get_label(0, 0, 16,
+        "You can changed the name of your <Data> folder.");
+    
     //--this is the seperator
-    let mut sep = frame::Frame::default().with_size(20, 2);
+    
+    let mut sep = sep();
+    flow.rule(&hdg,"^/<");
+    flow.rule(&hdg_sep,"=<^");
 
+    flow.rule(&change_data_btn, "^<");
+    flow.rule(&inp, "^<=>");
+    flow.rule(&explain, "^<=>");
+    flow.rule(&sep, "=<^");
+    
+    //============== level two
 
     
-    flow.end();
-    win.end();
-    win.resizable(&flow);
-    win.show();
-    
-    sep.set_color(Color::Black);
-    sep.set_frame(FrameType::FlatBox);
-   
-    
-//=< means expand to left
-flow.rule(&inp, "^<=>");
-flow.rule(&btn_change_data, "^<");
-flow.rule(&sep, "=<^");
-flow.rule(&f, "=<^");
-    
-    a.run().unwrap();
+// -----------------------------------------   
+flow.end();
+// -----------------------------------------   
+change_data_btn.set_callback(move|_b|{
+    let msg = String::from(inp.value());
+    let response = event_handler(msg);
+    dialog::message(200, 200, response.as_str());
+
+});
+// -----------------------------------------   
+win.end();win.resizable(&flow);
+win.show();a.run().unwrap();
 }
 
-
-fn event_handler(msg:String){
-println!("event_handler message:: {}",msg);
-}
-
-fn written_text(txt:&str,size:i32)->frame::Frame{
-
-    let mut f = frame::Frame::default()
-        .with_label(txt);
-       f.set_label_size(size);
+fn heading()->frame::Frame{
+let mut f = frame::Frame::
+new(0,0,10,10,"Hulk Setting");
+// f.set_label("testing");
+f.set_label_size(32);
 f
+}
+fn get_label(w:i32,h:i32,size:i32,title:&'static str)->frame::Frame{
+let mut f = frame::Frame::
+new(0,0,w,h,title);
+// f.set_label("testing");
+f.set_label_size(size);
+f
+}
+fn change_data_btn(title:&str)->button::Button{
+    let mut b = button::Button::default().with_size(100, 30).with_label(title);
+    b  
+}
+fn sep()->frame::Frame{
+let mut f = frame::Frame::default().with_size(10, 1);
+f.set_color(Color::Black);
+f.set_frame(FrameType::FlatBox);
+f
+}
+
+fn event_handler(msg:String)->String{
+    println!("event handler:: {}",msg);
+    "all quit on western front".to_string()
 }
